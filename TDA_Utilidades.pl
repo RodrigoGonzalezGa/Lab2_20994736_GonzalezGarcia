438eecd22611d615d0a93da830079dbedf9d83d6
Rodrigo Gonzalez <rodrigo.gonzalez.ga@usach.cl>
2023-11-04 23:27:01 -0300

    funcion de vusqueda que retorna la lista que posee el id deseado

--- TDA_Utilidades.pl
+++ TDA_Utilidades.pl
@@ -2,3 +2,6 @@ memberp(V,[V|_]).
 memberp(H,[_|L]):-memberp(H,L).
 memberid(H,[[H|_]|_]).
 memberid(H,[_|L]):-memberid(H,L).
+%devuelve el valor el valor que tenga la id correcta
+memberid2(V,[[V,V2]|_],R):-R = [V,V2].
+memberid2(V,[_|L],R):-memberid(V,L,R).
