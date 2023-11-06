bb8a5be17e6ceb475d8337e30de0d35ecfc37cd7
Rodrigo Gonzalez <rodrigo.gonzalez.ga@usach.cl>
2023-11-05 22:44:40 -0300

    añadida funcion flowAddOption correctamente, verificando repetidos

--- TDA_Utilidades.pl
+++ TDA_Utilidades.pl
@@ -3,5 +3,5 @@ memberp(H,[_|L]):-memberp(H,L).
 memberid(H,[[H|_]|_]).
 memberid(H,[_|L]):-memberid(H,L).
 %devuelve el valor el valor que tenga la id correcta
-memberid2(V,[[V,V2]|_],R):-R = [V,V2].
+memberid2(V,[[V,V2]|_],R):-R = [V,V2],!.
 memberid2(V,[_|L],R):-memberid(V,L,R).
